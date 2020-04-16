5b290ea3293c8c8677dd81b4bea94c4224d0924c
carletonDev <carletonDev@users.noreply.github.com>
2020-04-16 15:24:14 -0500

    setting up hibernate

--- electronicsStore/src/main/java/com/example/electronicsStore/ElectronicsStoreApplication.java
+++ electronicsStore/src/main/java/com/example/electronicsStore/ElectronicsStoreApplication.java
@@ -1,5 +1,7 @@
 package com.example.electronicsStore;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,9 @@ public class ElectronicsStoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronicsStoreApplication.class, args);
 	}
+//	private static SessionFactory buildSessionFactory(){
+//		Configuration config = new Configuration();
+//		config.
+//	}
 
 }
