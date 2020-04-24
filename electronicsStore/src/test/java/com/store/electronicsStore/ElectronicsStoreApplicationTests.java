eb918370b7f18dfec689a6d16ce58e32ffa7938a
carletonDev <carletonDev@users.noreply.github.com>
2020-04-24 17:20:59 -0500

    created login view with password encoder

--- electronicsStore/src/test/java/com/store/electronicsStore/ElectronicsStoreApplicationTests.java
+++ electronicsStore/src/test/java/com/store/electronicsStore/ElectronicsStoreApplicationTests.java
@@ -1,14 +1,34 @@
 package com.store.electronicsStore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.store.electronicsStore.dataproviderfactories.DataProviderFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ElectronicsStoreApplicationTests {
 
+  @LocalServerPort
+  private int serverPort;
 
-  @Test
+  private WebDriver driver;
+
+  @BeforeTest
+  public void setupDriver(){
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+
+  }
+
+  @Test(dataProvider = "logins", dataProviderClass = DataProviderFactory.class)
   void contextLoads() {
+
   }
 
 }
---
+++ electronicsStore/src/test/java/com/store/electronicsStore/dataproviderfactories/DataProviderFactory.java
@@ -0,0 +1,36 @@
+package com.store.electronicsStore.dataproviderfactories;
+
+import com.store.electronicsStore.hibernate.pojos.Login;
+import com.store.electronicsStore.hibernate.repositories.LoginRepository;
+import java.util.ArrayList;
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.DataProvider;
+
+public class DataProviderFactory {
+
+  private LoginRepository loginRepository;
+
+  @Autowired
+  public DataProviderFactory(LoginRepository loginRepository) {
+    this.loginRepository = loginRepository;
+  }
+
+  @DataProvider(name = "login")
+  public Object[][] logins() {
+    return createLoginObjects(loginRepository);
+  }
+
+  private Object[][] createLoginObjects(
+      LoginRepository loginRepository) {
+    ArrayList<ArrayList<Object>> twoDArray = new ArrayList<>();
+
+    for (Login user : loginRepository.findAll()) {
+      twoDArray.add(new ArrayList<>(Collections.singletonList(user)));
+    }
+    return twoDArray.stream().map(ArrayList::toArray).toArray(Object[][]::new);
+  }
+
+
+}
